dbda650043d4c1ecd3cd7de9e0780ded495eac28
我们是 <78197555+mlspacetoon@users.noreply.github.com>
2021-02-05 18:38:32 +0700

    Update kapankah2.js

--- plugins/kapankah2.js
+++ plugins/kapankah2.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, command, text }) => {
   conn.reply(m.chat, `
-*Pertanyaan:* ${command} ${text}
-*Jawaban:* ${Math.floor(Math.random() * 10)} ${pickRandom(['detik', 'menit', 'jam', 'hari', 'minggu', 'bulan', 'tahun', 'dekade', 'abad'])} lagi ...
+*Pertanyaanmuh:* ${command} ${text}
+*Jawabanquh:* ${Math.floor(Math.random() * 10)} ${pickRandom(['detik', 'menit', 'jam', 'hari', 'minggu', 'bulan', 'tahun', 'dekade', 'abad'])} lagi ...
 `.trim(), m)
 }
 handler.help = ['', 'kah'].map(v => 'kapan' + v + ' <pertanyaan>')
 handler.tags = ['kerang']
-handler.command = /^kapan(kah)?/i
+handler.command = /^kapan(kah)?$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
@@ -23,4 +23,3 @@ module.exports = handler
 function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)]
 }
-
