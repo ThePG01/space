b408f98a8f436c68dc69f811e6c0e7f3cadcde7a
我们是 <78197555+mlspacetoon@users.noreply.github.com>
2021-02-05 18:36:58 +0700

    Update kapankah.js

--- plugins/kapankah.js
+++ plugins/kapankah.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn, text }) => {
   conn.reply(m.chat, `
-*Pertanyaan:* ${m.text}
-*Jawaban:* ${Math.floor(Math.random() * 10)} ${pickRandom(['detik', 'menit', 'jam', 'hari', 'minggu', 'bulan', 'tahun', 'dekade', 'abad'])} lagi ...
+*Pertanyaanmuh:* ${m.text}
+*Jawabanquh:* ${Math.floor(Math.random() * 10)} ${pickRandom(['detik', 'menit', 'jam', 'hari', 'minggu', 'bulan', 'tahun', 'dekade', 'abad'])} lagi ...
 `.trim(), m)
 }
 handler.help = ['', 'kah'].map(v => 'kapan' + v + ' <text>?')
 handler.tags = ['kerang']
 handler.customPrefix = /(\?$)/
-handler.command = /^kapan(kah)?/i
+handler.command = /^kapan(kah)?$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
